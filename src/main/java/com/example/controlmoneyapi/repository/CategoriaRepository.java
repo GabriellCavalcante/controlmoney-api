a0f62261fb9b2da7f4acbcbbf2ad2d6a3b4fbb6d
GabriellCavalcante <cavalcante.eng91@gmail.com>
2018-08-10 02:12:36 -0300

    Registering new category with the post method

--- src/main/java/com/example/controlmoneyapi/repository/CategoriaRepository.java
+++ src/main/java/com/example/controlmoneyapi/repository/CategoriaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.controlmoneyapi.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
 	
 
 }
--- src/main/java/com/example/controlmoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/controlmoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.example.controlmoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.controlmoneyapi.model.Categoria;
 import com.example.controlmoneyapi.repository.CategoriaRepository;
@@ -22,6 +31,21 @@ public class CategoriaResource {
 		return cr.findAll();
 	}
 	
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = cr.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+		.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		return cr.findById(codigo);
+	}
 	
 	
 
