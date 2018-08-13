3fa302b876e1eb459de5c75ac8ba824174ea8176
GabriellCavalcante <cavalcante.eng91@gmail.com>
2018-08-13 14:34:22 -0300

    Removing person with DELETE

--- src/main/java/com/example/controlmoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/controlmoneyapi/resource/CategoriaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.controlmoneyapi.event.RecursoCriadoEvent;
@@ -27,7 +29,7 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository cr;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -40,7 +42,7 @@ public class CategoriaResource {
 	// @ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = cr.save(categoria);
-		
+
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoria.getCodigo()));
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
@@ -48,12 +50,19 @@ public class CategoriaResource {
 
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Optional<Categoria>> buscarPeloCodigo(@PathVariable Long codigo) {
-			Optional<Categoria> categoria = cr.findById(codigo);
-			if(categoria.isPresent()) {
+		Optional<Categoria> categoria = cr.findById(codigo);
+		if (categoria.isPresent()) {
 			return ResponseEntity.ok(categoria);
-			
-		}else  {
+
+		} else {
 			return ResponseEntity.notFound().build();
 
-		}}}
-	
+		}
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		cr.deleteById(codigo);
+	}
+}
--- src/main/java/com/example/controlmoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/controlmoneyapi/resource/PessoaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.controlmoneyapi.event.RecursoCriadoEvent;
@@ -27,7 +29,7 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pr;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
@@ -40,7 +42,7 @@ public class PessoaResource {
 	// @ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pr.save(pessoa);
-		
+
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
@@ -56,5 +58,11 @@ public class PessoaResource {
 			return ResponseEntity.notFound().build();
 
 		}
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pr.deleteById(codigo);
 	}
 }
