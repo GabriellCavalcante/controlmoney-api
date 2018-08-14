2c4d869e376da00f816a38958b9df3138f5215ab
GabriellCavalcante <cavalcante.eng91@gmail.com>
2018-08-14 19:09:30 -0300

    Validating inconsistency

--- src/main/java/com/example/controlmoneyapi/exceptionhandler/ControlmoneyExceptionHandler.java
+++ src/main/java/com/example/controlmoneyapi/exceptionhandler/ControlmoneyExceptionHandler.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -54,6 +56,17 @@ public class ControlmoneyExceptionHandler extends ResponseEntityExceptionHandler
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
+
+	@ExceptionHandler({ConstraintViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(ConstraintViolationException ex,
+			WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+	
 
 	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
 		List<Erro> erros = new ArrayList<>();
