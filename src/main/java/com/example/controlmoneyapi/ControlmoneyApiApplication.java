83b02db41c572d9c8f1cecbdb1301b985a89155e
GabriellCavalcante <cavalcante.eng91@gmail.com>
2018-08-22 15:04:55 -0300

    Implementing security with Oauth2 and Password Flow, configuring JWT in the project and renewing the access token with refresh token

--- src/main/java/com/example/controlmoneyapi/ControlmoneyApiApplication.java
+++ src/main/java/com/example/controlmoneyapi/ControlmoneyApiApplication.java
@@ -2,6 +2,8 @@ package com.example.controlmoneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 
 @SpringBootApplication
 public class ControlmoneyApiApplication {
@@ -9,4 +11,5 @@ public class ControlmoneyApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ControlmoneyApiApplication.class, args);
 	}
+	
 }
